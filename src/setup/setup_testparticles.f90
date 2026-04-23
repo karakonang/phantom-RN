03b5ded3b143e23909c5cce6b24cef7c3062c463
Angelos Karakonstantakis <angelos@karakonang.xyz>
2026-04-23 12:07:33 +0200

    fix testparticle custom initial conditions.
    Now, testparticle respects custom init. position / velocity and do not have to be hard-coded in the source file anymore...

--- src/setup/setup_testparticles.f90
+++ src/setup/setup_testparticles.f90
@@ -57,147 +57,170 @@ contains
 !+
 !----------------------------------------------------------------
 subroutine setpart(id,npart,npartoftype,xyzh,massoftype,vxyzu,polyk,gamma,hfact,time,fileprefix)
- use timestep,       only:dtmax,tmax
- use options,        only:iexternalforce,alpha,alphamax,alphau,beta,nfulldump
- use units,          only:set_units
- use physcon,        only:solarm
- use io,             only:master
- use externalforces, only:iext_star,a
- use eos,            only:ieos
- use physcon,        only:pi
- use prompting,      only:prompt
- use vectorutils,    only:cross_product3D
- use part,           only:gr
- use infile_utils,   only:get_options
- use kernel,         only:hfact_default
- integer,           intent(in)    :: id
- integer,           intent(out)   :: npart
- integer,           intent(out)   :: npartoftype(:)
- real,              intent(out)   :: xyzh(:,:)
- real,              intent(out)   :: massoftype(:)
- real,              intent(out)   :: polyk,gamma,hfact
- real,              intent(inout) :: time
- character(len=20), intent(in)    :: fileprefix
- real,              intent(out)   :: vxyzu(:,:)
- integer :: i,ierr
- real    :: dr,h0,xyz0(3),rhat(3),r2,vcirc,rtan(3),period,r0,fac,omega,rkerr,z
+use timestep,       only:dtmax,tmax
+use options,        only:iexternalforce,alpha,alphamax,alphau,beta,nfulldump
+use units,          only:set_units
+use physcon,        only:solarm
+use io,             only:master
+use externalforces, only:iext_star,a,accradius1,accradius1_hard
+use metric,         only:charge_metric => charge
+use eos,            only:ieos
+use physcon,        only:pi
+use prompting,      only:prompt
+use vectorutils,    only:cross_product3D
+use part,           only:gr
+use infile_utils,   only:get_options
+use kernel,         only:hfact_default
+use metric_tools,   only:imetric,imet_rn
+integer,           intent(in)    :: id
+integer,           intent(out)   :: npart
+integer,           intent(out)   :: npartoftype(:)
+real,              intent(out)   :: xyzh(:,:)
+real,              intent(out)   :: massoftype(:)
+real,              intent(out)   :: polyk,gamma,hfact
+real,              intent(inout) :: time
+character(len=20), intent(in)    :: fileprefix
+real,              intent(out)   :: vxyzu(:,:)
+integer :: i,ierr
+real    :: dr,h0,xyz0(3),rhat(3),r2,vcirc,rtan(3),period,r0,fac,omega,rkerr,z,m1
 
- call set_units(mass=solarm,G=1.d0,c=1.d0)
- !
- ! default runtime parameters
- !
- orbtype = 1
- spin = 0.
- r = 10.
- norbits = 1.
- dumpsperorbit = 100
- x0 = 0.
- y0 = 0.
- z0 = 0.
- vx0 = 0.
- vy0 = 0.
- vz0 = 0.
- tmax = 500.
- dtmax = 0.5
- period = tmax
- hfact = hfact_default
- !
- ! read runtime parameters from setup file
- !
- if (id==master) print "(/,65('-'),1(/,a),/,65('-'),/)",' Test particles setup'
- call get_options(trim(fileprefix)//'.setup',id==master,ierr,&
-                  read_setupfile,write_setupfile,setup_interactive)
- if (ierr /= 0) stop 'rerun phantomsetup after editing .setup file'
- !
- ! general parameters
- !
- time  = 0.
- if (gr) then
-    gamma = 5./3. ! GR cannot have gamma=1
- else
-    gamma = 1.
- endif
- polyk = 0.
- npart = 10
- ieos  = 11
- nfulldump = 1
- alpha     = 0.
- alphamax  = 0.
- alphau    = 0.
- beta      = 0.
- massoftype     = 1.e-10
- npartoftype(:) = 0
- npartoftype(1) = npart
+call set_units(mass=solarm,G=1.d0,c=1.d0)
+!
+! default runtime parameters
+!
+orbtype = 1
+spin = 0.
+charge = 0.
+test_mass = 1.e-10
+r = 10.
+norbits = 1.
+dumpsperorbit = 100
+x0 = 0.
+y0 = 0.
+z0 = 0.
+vx0 = 0.
+vy0 = 0.
+vz0 = 0.
+tmax = 500.
+dtmax = 0.5
+period = tmax
+hfact = hfact_default
+!
+! read runtime parameters from setup file
+!
+if (id==master) print "(/,65('-'),1(/,a),/,65('-'),/)",' Test particles setup'
+call get_options(trim(fileprefix)//'.setup',id==master,ierr,&
+                 read_setupfile,write_setupfile,setup_interactive)
+if (ierr /= 0) stop 'rerun phantomsetup after editing .setup file'
+!
+! general parameters
+!
+time  = 0.
+if (gr) then
+   gamma = 5./3. ! GR cannot have gamma=1
+else
+   gamma = 1.
+endif
+polyk = 0.
+npart = 10
+ieos  = 11
+nfulldump = 1
+alpha     = 0.
+alphamax  = 0.
+alphau    = 0.
+beta      = 0.
+massoftype     = test_mass
+npartoftype(:) = 0
+npartoftype(1) = npart
 
- xyzh  = 0.
- vxyzu = 0.
+xyzh  = 0.
+vxyzu = 0.
 
- ! Calculate orbit parameters based on orbit type
- select case(orbtype)
- case(1,3,4) ! circular, epicycle, vertical oscillation
-    omega = 1./(r**(1.5)+spin)
-    x0 = sqrt(r**2 + spin**2)
-    vy0 = x0*omega
-    period = 2.*pi/omega
-    if (orbtype == 3) then ! epicycle
-       fac = 1.00001
-       vy0 = fac*x0*omega
-    elseif (orbtype == 4) then ! vertical oscillation
-       fac = 1.00001
-       z0  = fac-1.
-    endif
- case(2) ! precession
-    x0     = 90.
-    vy0    = 0.0521157
-    period = 2.*pi*sqrt((0.5*x0)**3/1.) ! approximate
- case(0) ! custom
-    x0  = 10.
-    vy0 = sqrt(1./x0)
- end select
+m1 = 1.0 ! Default central mass
 
- if (orbtype > 0) then
-    tmax   = norbits*period
-    dtmax  = period/dumpsperorbit
- endif
+! Calculate orbit parameters based on orbit type
+select case(orbtype)
+case(1,3,4) ! circular, epicycle, vertical oscillation
+   if (gr .and. imetric == imet_rn) then
+      omega = sqrt(max(0., m1/r**3 - charge**2/r**4))
+   else
+      omega = 1./(r**(1.5)+spin)
+   endif
+   x0 = sqrt(r**2 + spin**2)
+   vy0 = x0*omega
+   period = 2.*pi/(omega + epsilon(omega))
+   if (orbtype == 3) then ! epicycle
+      fac = 1.00001
+      vy0 = fac*x0*omega
+   elseif (orbtype == 4) then ! vertical oscillation
+      fac = 1.00001
+      z0  = fac-1.
+   endif
+case(2) ! precession
+   x0     = 90.
+   vy0    = 0.0521157
+   period = 2.*pi*sqrt((0.5*x0)**3/1.) ! approximate
+case(0) ! custom
+   ! Values should have been read from setup file or interactive
+end select
 
- print*,''
- print*,' Setup for single test particle: '
- print*,' tmax = ',tmax
- print*,' Initial  (x,y,z)   = ',x0,y0,z0
- print*,' Initial (vx,vy,vz) = ',vx0,vy0,vz0
- print*,''
+if (orbtype > 0) then
+   tmax   = norbits*period
+   dtmax  = period/dumpsperorbit
+endif
 
- xyzh(1:3,1)   = (/x0,y0,z0/)
- vxyzu(1:3,1)  = (/vx0,vy0,vz0/)
- !
- ! Put all other particles in a radial line outwards from the origin, with their circular velocity,
- ! but only in the x-y plane. Also set smoothing lengths, and thermal energies.
- !
- xyz0          = (/xyzh(1,1),xyzh(2,1),0./)
- r0            = sqrt(dot_product(xyz0,xyz0))
- rhat          = xyz0/r0
- dr            = 0.25
- h0            = 10.*dr
- xyzh(4,:)     = h0
- vxyzu(4,:)    = 0.
- do i=2,npart
-    xyzh(1:3,i) = xyz0 + (i-1)*dr*rhat
-    call cross_product3D((/0.,0.,1./),xyzh(1:3,i),rtan)          ! Vector tangential to motion
-    rtan  = rtan/sqrt(dot_product(rtan,rtan))                    ! Unit vector tangential to motion
-    r2    = xyzh(1,i)**2 + xyzh(2,i)**2 + xyzh(3,i)**2
-    z     = xyzh(3,i)
-    rkerr = sqrt((r2-spin**2)/2. + sqrt((r2-spin**2)**2 + 4.*spin**2*z**2)/2.)
-    omega = 1./(rkerr**(1.5)+spin)
-    x0    = sqrt(rkerr**2 + spin**2)
-    vcirc = x0*omega
-    vxyzu(1:3,i) = rtan*vcirc
- enddo
+print*,''
+print*,' Setup for single test particle: '
+print*,' tmax = ',tmax
+print*,' Initial  (x,y,z)   = ',x0,y0,z0
+print*,' Initial (vx,vy,vz) = ',vx0,vy0,vz0
+print*,''
 
- a = spin
- if (.not.gr) iexternalforce = iext_star
+xyzh(1:3,1)   = (/x0,y0,z0/)
+vxyzu(1:3,1)  = (/vx0,vy0,vz0/)
+!
+! Put all other particles in a radial line outwards from the origin, with their circular velocity,
+! but only in the x-y plane. Also set smoothing lengths, and thermal energies.
+!
+xyz0          = (/xyzh(1,1),xyzh(2,1),0./)
+r0            = sqrt(dot_product(xyz0,xyz0))
+if (r0 > 0.) then
+   rhat          = xyz0/r0
+else
+   rhat          = (/1.,0.,0./)
+endif
+dr            = 0.25
+h0            = 10.*dr
+xyzh(4,:)     = h0
+vxyzu(4,:)    = 0.
+do i=2,npart
+   xyzh(1:3,i) = xyz0 + (i-1)*dr*rhat
+   call cross_product3D((/0.,0.,1./),xyzh(1:3,i),rtan)          ! Vector tangential to motion
+   r2    = xyzh(1,i)**2 + xyzh(2,i)**2 + xyzh(3,i)**2
+   r0    = sqrt(r2)
+   if (r2 > 0. .and. rtan(1)**2 + rtan(2)**2 + rtan(3)**2 > 0.) then
+      rtan  = rtan/sqrt(dot_product(rtan,rtan))                    ! Unit vector tangential to motion
+      z     = xyzh(3,i)
+      if (gr .and. imetric == imet_rn) then
+         omega = sqrt(max(0., m1/r0**3 - charge**2/r0**4))
+         vcirc = r0*omega
+      else
+         rkerr = sqrt((r2-spin**2)/2. + sqrt((r2-spin**2)**2 + 4.*spin**2*z**2)/2.)
+         omega = 1./(rkerr**(1.5)+spin)
+         rkerr = sqrt(rkerr**2 + spin**2)
+         vcirc = rkerr*omega
+      endif
+      vxyzu(1:3,i) = rtan*vcirc
+   else
+      vxyzu(1:3,i) = 0.
+   endif
+enddo
+
+a = spin
+charge_metric = charge
+if (.not.gr) iexternalforce = iext_star
 
 end subroutine setpart
-
 !-----------------------------------------------------------------------
 !+
 !  Write setup parameters to .setup file
@@ -213,6 +236,8 @@ subroutine write_setupfile(filename)
  write(iunit,"(a)") '# input file for test particles setup'
  call write_inopt(orbtype,'orbtype','orbit type (1=circle, 2=precession, 3=epicycle, 4=vertical-oscillation, 0=custom)',iunit)
  call write_inopt(spin,'spin','black hole spin',iunit)
+ call write_inopt(charge,'charge','black hole charge',iunit)
+ call write_inopt(test_mass,'test_mass','test particle mass',iunit)
  select case(orbtype)
  case(1,3,4) ! circular, epicycle, vertical oscillation
     call write_inopt(r,'r','initial radius r in spherical coordinates',iunit)
@@ -249,13 +274,14 @@ subroutine read_setupfile(filename,ierr)
  ierr = 0
  print "(a)",' reading setup options from '//trim(filename)
  call open_db_from_file(db,filename,iunit,ierr)
+ if (ierr /= 0) return
  call read_inopt(orbtype,'orbtype',db,min=0,max=4,errcount=nerr)
  call read_inopt(spin,'spin',db,min=-1.,max=1.,errcount=nerr)
+ call read_inopt(charge,'charge',db,errcount=nerr)
+ call read_inopt(test_mass,'test_mass',db,errcount=nerr)
  select case(orbtype)
  case(1,3,4) ! circular, epicycle, vertical oscillation
     call read_inopt(r,'r',db,min=0.,errcount=nerr)
-    call read_inopt(norbits,'norbits',db,min=0.,errcount=nerr)
-    call read_inopt(dumpsperorbit,'dumpsperorbit',db,min=1,errcount=nerr)
  case default ! custom
     call read_inopt(x0,'x0',db,errcount=nerr)
     call read_inopt(y0,'y0',db,errcount=nerr)
@@ -285,7 +311,11 @@ subroutine setup_interactive()
  use prompting, only:prompt
  use part,      only:gr
 
- if (gr) call prompt('black hole spin',spin,-1.,1.)
+ if (gr) then
+    call prompt('black hole spin',spin,-1.,1.)
+    call prompt('black hole charge',charge)
+ endif
+ call prompt('test particle mass',test_mass)
  call prompt('select orbit type (1=circle, 2=precession, 3=epicycle, 4=vertical-oscillation, 0=custom)',orbtype,0,4)
  select case(orbtype)
  case(1,3,4) ! circular, epicycle, vertical oscillation
